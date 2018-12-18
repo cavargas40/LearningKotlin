f85cfd54e4e604b6fd0fd012f6cb8cbaaf8b5120
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2018-12-18 17:54:58 -0500

    first commit

---
+++ src/com/cv/learningkotlin/java/BoringJavaCode.java
@@ -0,0 +1,7 @@
+package com.cv.learningkotlin.java;
+
+public class BoringJavaCode {
+    public static void main (String args[]){
+        String name = "Carlos";
+    }
+}
---
+++ src/com/cv/learningkotlin/kotlin/ExploringVariables.kt
@@ -0,0 +1,11 @@
+package com.cv.learningkotlin.kotlin
+
+fun main(args: Array<String>) {
+    var name: String = "Carlos"
+    val surname = "Vargas"
+
+    name = "Andres"
+    //surname = "Lopez" //not valid for the val
+
+    println("$name $surname")
+}
