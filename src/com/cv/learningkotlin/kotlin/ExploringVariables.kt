5a53103d9a4381220f042b4cbc8598a0440faf19
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2018-12-18 18:27:02 -0500

    added exploring variables (Strings)

--- src/com/cv/learningkotlin/kotlin/ExploringVariables.kt
+++ src/com/cv/learningkotlin/kotlin/ExploringVariables.kt
@@ -1,11 +1,31 @@
 package com.cv.learningkotlin.kotlin
 
 fun main(args: Array<String>) {
+    strings()
+}
+
+fun strings() {
     var name: String = "Carlos"
     val surname = "Vargas"
 
     name = "Andres"
     //surname = "Lopez" //not valid for the val
 
-    println("$name $surname")
+    println("$name ${surname.toUpperCase()}") //template literals
+    println("Your first name has ${name.length} characters")
+    println("Your product cost \$about10")
+
+    //multiple lines string
+    val story = """It was a dark and stormy night.
+        |A foul smell crept across the city.
+        |Jane wondered what time it was, and when it would be daylight.""".trimMargin("|")
+
+    println(story)
+
+    val changedStory = story.replaceAfterLast("it", " would be dawn")
+
+    println(changedStory)
 }
+
+
+
