d0519866ea423ce24d9e7bed9ab90719e8fc0b1d
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2019-01-02 17:33:38 -0500

    added exploring exceptions

---
+++ src/com/cv/learningkotlin/kotlin/ExploringExceptions.kt
@@ -0,0 +1,30 @@
+package com.cv.learningkotlin.kotlin
+
+import java.io.FileInputStream
+
+@Throws (InterruptedException::class)
+fun divide(a: Int, b: Int): Double {
+    Thread.sleep(1000)
+    return (a.toDouble() / b)
+}
+
+fun printFile(){
+    val input = FileInputStream("file.txt")
+
+    input.use {
+        //an exception could be thrown here
+    }
+}
+
+fun main(args: Array<String>) {
+
+    var result = try {
+        divide(6,3)
+    } catch (e: Exception){
+        println(e)
+        0
+    }
+
+    println(result)
+
+}
